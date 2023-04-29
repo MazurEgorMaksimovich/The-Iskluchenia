7027c31309a51804bda6c0a7c11f39fc4b79cc4f
MazurEgorMaksimovich <egormazur2008@gmail.com>
2023-04-29 15:55:58 +0300

    Add 11th task.

---
+++ The_Iskluchenia-11.py
@@ -0,0 +1,84 @@
+"""
+Помилки (номери рядків через пробіл): !!!
+"""
+
+
+class NoMoneyToWithdrawError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class PaymentError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class Zashchita_ot_Obyegoroviniya(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def print_accounts(accounts):
+    """Печать аккаунтов."""
+    print("Список клиентов ({}): ".format(len(accounts)))
+    for i, (name, value) in enumerate(accounts.items(), start=1):
+        print("{}. {} {}".format(i, name, value))
+
+
+def transfer_money(accounts, account_from, account_to, value):
+    try:
+        if value < 0:
+            raise Zashchita_ot_Obyegoroviniya("Нельзя перевести отрицательную сумму деняк.")
+        elif value > accounts[account_from]:
+            raise NoMoneyToWithdrawError("Недостаточно средств для совершения транзакции.")
+        else:
+            account_from_money = accounts[account_from]
+            account_to_money = accounts[account_to]
+            try:
+                accounts[account_from] -= value
+                accounts[account_to] += value
+            except:
+                accounts[account_from] = account_from_money
+                accounts[account_to] = account_to_money
+                raise PaymentError
+    except Zashchita_ot_Obyegoroviniya as err:
+        print()
+        print(str(err))
+        print()
+    except NoMoneyToWithdrawError as err:
+        print()
+        print(str(err))
+        print()
+    except Exception:
+        print()
+        print("Произошла неизвестная ошибка.")
+        print()
+
+
+if __name__ == "__main__":
+    accounts = {
+        "Вася Пупкин": 100,
+        "Вася Табуреткин": 1500,
+        "Вася Васильков": 400
+    }
+    print_accounts(accounts)
+
+    payment_info = {
+        "account_from": "Вася Пупкин",
+        "account_to": "Вася Табуреткин"
+    }
+
+    print("Перевод от пользователя {account_from} пользователю {account_to}...".
+          format(**payment_info))
+
+    try:
+        payment_info["value"] = int(input("Сумма = "))
+
+        transfer_money(accounts, **payment_info)
+
+        print("OK!")
+    except ValueError:
+        print()
+        print("Нерекомендуется пользоваться банковскими услугами в нетрезвом состоянии. Проверьте вводные данные.")
+        print()
+
+print_accounts(accounts)
